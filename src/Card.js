941af9b2ec35813a8738bbc968292a8bc3bd7926
Blade Boles <bladerboles@gmail.com>
2020-01-03 12:11:06 -0700

    Making progress, but handleDeleteItem still is not working as intended

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Card.css';
 
 export default function Card(props) {
+  
+  Card.defaultProps = {
+    key: '',
+    id: '',
+    title: '',
+    items: {},
+    content: ''
+  }
+
   return (
     <div className='Card'>
       <button
         type='button'
+        onClick={() => props.deleteCallback(props.id)}
       >
         delete
       </button>
--- src/List.js
+++ src/List.js
@@ -3,6 +3,15 @@ import Card from './Card'
 import './List.css';
 
 export default function List(props) {
+  
+  List.defaultProps = {
+    key: '',
+    id: '',
+    title: '',
+    items: {},
+    content: ''
+  }
+  
   return (
     <section className='List'>
       <header className='List-header'>
@@ -12,17 +21,24 @@ export default function List(props) {
         {props.cards.map((card) =>
           <Card
             key={card.id}
+            id={card.id}
             title={card.title}
+            items={props.items}
             content={card.content}
+            deleteCallback={props.deleteCallback}
           />
         )}
         <button
           type='button'
           className='List-add-button'
+          onClick={props.randomCallback}
         >
           + Add Random Card
         </button>
       </div>
     </section>
   )
+
+  
+
 }
